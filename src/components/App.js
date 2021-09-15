c0b3b60155a8580bf771936f107a14fed73058c4
Masi <51009658+MasiMasinga@users.noreply.github.com>
2021-09-15 08:41:55 +0200

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import SignUp from "./SignUp";
+
+function App() {
+  return (
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <SignUp className="w-100" style={{ maxWidth: "400px" }} />
+    </Container>
+  )
+}
+
+export default App;

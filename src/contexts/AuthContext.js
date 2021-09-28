6aa7343518e926054441b739bf3c512dcc0a57ed
Masi <51009658+MasiMasinga@users.noreply.github.com>
2021-09-29 00:12:12 +0200

    Add files via upload

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "../firebase"
 
-const AuthContext = React.createContext();
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
 
 export function AuthProvider({ children }) {
-    return (
-        <AuthContext.Provider>
-            {children}
-        </AuthContext.Provider>
-    );
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
+
+  function logout() {
+    return auth.signOut()
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email)
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
 }
